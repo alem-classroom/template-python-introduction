a4931c821685b9d36dfc078915c0d55a04291329
Saken <35443199+sakenism@users.noreply.github.com>
2020-10-13 12:10:48 +0600

    Update lists.py

--- lists/lists.py
+++ lists/lists.py
@@ -6,6 +6,7 @@ def add_elem_to_list(list, elem):
 
 def delete_elem_from_list(list, index = -1):
     # delete element from list, such that its index is index
+    # if index is invalid, return empty list
 
 def count_elements_in_list(list, x):
     # count elements in the list that are equal to x and return the count
